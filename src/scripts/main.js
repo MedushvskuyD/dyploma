b93eb99ab5787dc90450d2ea0fb7af959c633fe3
Deman250 <deman.250052@gmail.com>
2020-03-24 20:41:27 +0200

    no adaptive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,32 @@ $(document).ready(function() {
   $('#slider').slick({
     nextArrow: '<button type="button" class="slick slick-next"></button>',
     prevArrow: '<button type="button" class="slick slick-prev"></button>',
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    // slidesToShow: 2,
+    // infinite: false,
+  });
+
+  $('#offerSlider').slick({
+    nextArrow: '<button type="button" class="slick slick-next"></button>',
+    prevArrow: '<button type="button" class="slick slick-prev"></button>',
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    // infinite: false,
+  });
+
+  $('#reviewSlider').slick({
+    nextArrow: '<button type="button" class="slick slick-next"></button>',
+    prevArrow: '<button type="button" class="slick slick-prev"></button>',
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
     // infinite: false,
   });
 });
